5fe048dca7c020984b8c471f348d0ce23fd786af
Dan Politica <73758508+benochi@users.noreply.github.com>
2023-10-25 08:46:31 -0500

    add fourth weight and input

--- p1.py
+++ p1.py
@@ -1,8 +1,11 @@
 #neuron example
-inputs = [1, 2, 3]
-weight = [0.2, 0.8, -0.5]
+inputs = [1, 2, 3, 2.5]
+weight = [0.2, 0.8, -0.5, 1.0]
 bias = 2
 
+
+
+
 #all inputs * weights + bias
-output = inputs[0]*weight[0] + inputs[1]*weight[1] + inputs[2]*weight[2] + bias
+output = inputs[0]*weight[0] + inputs[1]*weight[1] + inputs[2]*weight[2] + inputs[3]*weight[3] + bias
 print(output)
