a57d3cc84b03b9eb55f8a537189714057358f6cb
Dan Politica <73758508+benochi@users.noreply.github.com>
2023-10-25 08:35:42 -0500

    neuron example

---
+++ p1.py
@@ -0,0 +1,8 @@
+#neuron example
+inputs = [1.2, 5.1, 2.1]
+weight = [3.1, 2.1, 8.7]
+bias = 3
+
+#all inputs * weights + bias
+output = inputs[0]*weight[0] + inputs[1]*weight[1] + inputs[2]*weight[2] + bias
+print(output)
