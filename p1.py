eae04a1bdd553297c6bc6fd8f55e31d7ce20af38
Dan Politica <73758508+benochi@users.noreply.github.com>
2023-10-25 08:42:14 -0500

    update values

--- p1.py
+++ p1.py
@@ -1,7 +1,7 @@
 #neuron example
-inputs = [1.2, 5.1, 2.1]
-weight = [3.1, 2.1, 8.7]
-bias = 3
+inputs = [1, 2, 3]
+weight = [0.2, 0.8, -0.5]
+bias = 2
 
 #all inputs * weights + bias
 output = inputs[0]*weight[0] + inputs[1]*weight[1] + inputs[2]*weight[2] + bias
