afdeb1d2b0ad35ebe37aeebfecf12cab23d09ea8
Dan Politica <73758508+benochi@users.noreply.github.com>
2023-10-25 11:26:12 -0500

    shapes

---
+++ shape.py
@@ -0,0 +1,11 @@
+#list l =[1,2,3,4] shape = (4), = 1d array, vector
+#list l =[[1,5,6,2],[3,2,1,3]] shape = (2,4)  = 2d array, matrix(rectangular array, list of vectors = Matrix) 
+#arrays must have the same size at each element homologous
+
+# #lolol =           Type = 3d array shape = (3,2,4)
+# [[[1,5,6,2],
+#   [3,2,1,3]],
+#  [[5,2,1,2],
+#   [6,4,8,4]],
+#  [[2,8,5,3],
+#   [1,1,9,4]]]
