20b97d1c963b9c9ba9e16cf25ee71e8b52b5e0de
Dan Politica <73758508+benochi@users.noreply.github.com>
2023-10-25 08:53:07 -0500

    example output for 3 neurons.

--- p1.py
+++ p1.py
@@ -1,11 +1,17 @@
 #neuron example
 inputs = [1, 2, 3, 2.5]
-weight = [0.2, 0.8, -0.5, 1.0]
-bias = 2
 
+weight1 = [0.2, 0.8, -0.5, 1.0]
+weight2 = [0.5, -0.91, 0.26, -0.5]
+weight3 = [-0.26, -0.27, 0.17, 0.87]
 
+bias1 = 2
+bias2 = 3
+bias3 = 0.5
 
 
 #all inputs * weights + bias
-output = inputs[0]*weight[0] + inputs[1]*weight[1] + inputs[2]*weight[2] + inputs[3]*weight[3] + bias
+output = [inputs[0]*weight1[0] + inputs[1]*weight1[1] + inputs[2]*weight1[2] + inputs[3]*weight1[3] + bias1,
+          inputs[0]*weight2[0] + inputs[1]*weight2[1] + inputs[2]*weight2[2] + inputs[3]*weight2[3] + bias2,
+          inputs[0]*weight3[0] + inputs[1]*weight3[1] + inputs[2]*weight3[2] + inputs[3]*weight3[3] + bias3]
 print(output)
