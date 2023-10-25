2f1d931e443b0538a7cd5d735c51187342bd9a04
Dan Politica <73758508+benochi@users.noreply.github.com>
2023-10-25 11:42:52 -0500

    numpy

--- p1.py
+++ p1.py
@@ -1,10 +1,6 @@
 #neuron example
+import numpy as np
 inputs = [1, 2, 3, 2.5] #inputs don't change, you change weights.  inputs represent data passed into NN. 
-
-weight1 = [0.2, 0.8, -0.5, 1.0]
-weight2 = [0.5, -0.91, 0.26, -0.5]
-weight3 = [-0.26, -0.27, 0.17, 0.87]
-
 weights = [
             [0.2, 0.8, -0.5, 1.0], 
             [0.5, -0.91, 0.26, -0.5], 
@@ -13,11 +9,20 @@ weights = [
 
 biases = [2, 3, 0.5]
 
-layer_outputs = []
-for neuron_weights, neuron_bias in zip(weights, biases):
-  neuron_output = 0
-  for n_input, weight in zip(inputs, neuron_weights):
-    neuron_output += neuron_bias
-    layer_outputs.append(neuron_output)
 
-print(layer_outputs)
+output = np.dot(weights, inputs) + biases #if putting inputs first will get shape error. 
+
+print(output)
+
+
+
+
+
+# layer_outputs = []
+# for neuron_weights, neuron_bias in zip(weights, biases):
+#   neuron_output = 0
+#   for n_input, weight in zip(inputs, neuron_weights):
+#     neuron_output += neuron_bias
+#     layer_outputs.append(neuron_output)
+
+# print(layer_outputs)
