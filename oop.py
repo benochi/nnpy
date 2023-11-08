3116897566f6a2be8297742122cf2fdc70cc590a
Dan Politica <73758508+benochi@users.noreply.github.com>
2023-11-08 09:42:22 -0600

    notes on weights and biases

--- oop.py
+++ oop.py
@@ -1,5 +1,17 @@
 import numpy as np
 
-inputs = [[1, 2, 3, 2.5], [2.0, 5.0, -1.0, 2.0], [-1.5, 2.7, 3.3, -0.8]]
+inputs = [
+    [1, 2, 3, 2.5],
+    [2.0, 5.0, -1.0, 2.0],
+    [-1.5, 2.7, 3.3, -0.8],
+]  # 3 samples of input data
 
-# define hidden layers
+
+# when loading a model, just saving weights and biases. with new NN need to initialize weights and biases - weights random values between 1 and -1, tighter range is better.
+# tighter range means thing should tend between 1, -1 so input values will hopefully not explode the range. -0.1 - 0.1 for weights, biases tend to be initialized as zero.
+class Layer_Dense:
+    def __init__(self):
+        pass
+
+    def forward(self):
+        pass
