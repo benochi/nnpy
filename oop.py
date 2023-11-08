9216774321bcba72024822a0a5fe772a2608d0b7
Dan Politica <73758508+benochi@users.noreply.github.com>
2023-11-08 09:36:27 -0600

    layers added

--- oop.py
+++ oop.py
@@ -6,7 +6,16 @@ weights = [[0.2, 0.8, -0.5, 1.0], [0.5, -0.91, 0.26, -0.5], [-0.26, -0.27, 0.17,
 
 biases = [2, 3, 0.5]
 
+# layer 2
+weights2 = [[0.1, -0.14, 0.5], [-0.5, 0.12, -0.33], [-0.44, 0.73, -0.13]]
+
+biases2 = [-1, 2, -0.5]
+
 # weights need to be transposed, rows become columns NP requires an array.
 # transpose takes the 3 lists of 4 length and makes 4 rows of 3 length, so shape matches.
-output = np.dot(inputs, np.array(weights).T) + biases
-print(output)
+layer1_outputs = np.dot(inputs, np.array(weights).T) + biases
+# layer 1 outputs get put into layer 2 with weights 2 and biases 2
+layer2_outputs = np.dot(layer1_outputs, np.array(weights2).T) + biases2
+
+
+print(layer2_outputs)
