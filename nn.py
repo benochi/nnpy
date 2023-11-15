d09cc0fa4945b7168cc1e8f4c966b607c36cc4d6
Dan Politica <73758508+benochi@users.noreply.github.com>
2023-11-15 15:47:56 -0600

    correction for loss added

--- nn.py
+++ nn.py
@@ -26,6 +26,27 @@ class Activation_softmax:
         self.output = probabilities
 
 
+class Loss:
+    def calculate(self, output, y):
+        sample_losses = self.forward(output, y)  # will work with all forwards
+        data_loss = np.mean(sample_losses)  # batch loss
+        return data_loss
+
+
+class Loss_CategoricalCrossentropy(Loss):
+    def forward(self, y_pred, y_true):
+        samples = len(y_pred)
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)  # handle divide by zero error
+        # handle scalar and one hot zero values. [1,0] vs [[0,1],[1,0]]
+        if len(y_true.shape) == 1:  # this means scalar
+            correct_confidences = y_pred_clipped[range(samples), y_true]
+        elif len(y_true.shape) == 2:  # one hot encoder
+            correct_confidences = np.sum(y_pred_clipped * y_true, axis=1)
+
+        negative_log_likelihoods = -np.log(correct_confidences)
+        return negative_log_likelihoods
+
+
 X, y = spiral_data(samples=100, classes=3)
 
 dense1 = Layer_Dense(2, 3)
@@ -38,7 +59,10 @@ activation1.forward(dense1.output)
 dense2.forward(activation1.output)
 activation2.forward(dense2.output)
 
-print(activation2.output[:10])
+loss_function = Loss_CategoricalCrossentropy()
+loss = loss_function.calculate(activation2.output, y)
+
+print("Loss: ", loss)
 
 # my own code to find highest variance in entire dataset
 # output = []
